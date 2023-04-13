f5a95f145a1e062dfb2fde3e7fa21235062dcc93
maggi-bratton <maggi.meins@gmail.com>
2023-04-13 10:48:03 -0600

    Final project from codesandbox

---
+++ src/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Header = (props) => {
+  const callSearch = (e) => {
+    props.onSearch(e.target.value);
+  };
+
+  return (
+    <header>
+      <h1 className="app-header__title">Super Sticky Notes</h1>
+      <aside className="app-header__controls">
+        <button className="add-new" onClick={props.addNote}>
+          + New Note
+        </button>
+        <input
+          value={props.searchText}
+          className="search"
+          type="text"
+          placeholder="Type here to search..."
+          onChange={callSearch}
+        />
+      </aside>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/Note.js
@@ -0,0 +1,43 @@
+import React from "react";
+import NotesList from "./NotesList";
+
+const Note = (props) => {
+  const updateTitle = (e) => {
+    const updatedValue = e.target.value;
+    const editMeId = props.note.id;
+    props.onType(editMeId, "title", updatedValue);
+  };
+
+  const updateDescription = (e) => {
+    const updatedValue = e.target.value;
+    const editMeId = props.note.id;
+    props.onType(editMeId, "descirption", updatedValue);
+  };
+
+  const clickDelete = () => {
+    props.removeNote(props.note.id);
+  };
+
+  return (
+    <li className="note">
+      <input
+        className="note__title"
+        type="text"
+        placeholder="Title"
+        value={props.note.title}
+        onChange={updateTitle}
+      />
+      <textarea
+        className="note__description"
+        placeholder="Description..."
+        value={props.note.description}
+        onChange={updateDescription}
+      />
+      <span className="note__delete" onClick={clickDelete}>
+        X
+      </span>
+    </li>
+  );
+};
+
+export default Note;
